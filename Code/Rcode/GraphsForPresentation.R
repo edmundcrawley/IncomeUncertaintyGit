2642ec7b0d2fe679d6b904827f365d2bdcb7481d
edmundcrawley <edmundcrawley@gmail.com>
2019-04-12 11:35:46 -0400

    Improvements to slides

---
+++ Code/Rcode/GraphsForPresentation.R
@@ -0,0 +1,108 @@
+
+#################################################################################
+# 
+# This file makes graphs for the presentation
+# Nicer colors and larger fonts
+#
+# 
+###############################################################################
+
+thickness = 2
+plot_width = 9.5
+right_mar = 17
+left_mar = 7
+col_complete = "cyan"
+col_solow = "blue"
+col_bs = "red"
+
+
+###############################################################################
+# Pull in consumption saving numbers from Python output
+FromPython <- scan(paste(PythonResults_folder,'basic_regressions.txt',sep=''), what=double(), sep=",")
+FromPython <- scan(paste(PythonResults_folder,'benchmark_br_all.txt',sep=''), what=double(), sep=",")
+solow_spending = 0.75
+# Now draw graph
+#png(paste(figures_dir, "basic_regression_complete_slides",tag,".png",sep=""))
+pdf(paste(figures_dir, "basic_regression_complete_slides",tag,".pdf",sep=""),width=plot_width)
+#svg(paste(figures_dir, "basic_regression_complete_slides",tag,".svg",sep=""))
+par(mar=c(8,left_mar,4,right_mar)+0.1,cex.axis=1.2,cex.lab=1.5,xpd=TRUE)
+plot(array(0.0,dim=dim(reg_coefs)), ylim=c(0,1),lty='solid', lwd=thickness,col=col_complete,type='o', pch=15, las=1,xlab='N, Years of Growth',ylab='',main='Regressing Consumption Growth on Income Growth')
+mtext(TeX('$\\beta^N$'),side=2,las=1,line=4, cex = 1.5)
+legend("right", inset=c(-0.6,0), legend=c("Complete Markets","","",""), col=c(col_complete,col_solow,col_bs,"black"),lty=c("solid","solid","solid","solid"),lwd=c(thickness,thickness,thickness,thickness), pch=c(15,16,17,15), pt.cex = 1, cex = 1.3, y.intersp=1.3,bty="n")
+dev.off()
+
+#png(paste(figures_dir, "basic_regression_solow_slides",tag,".png",sep=""))
+pdf(paste(figures_dir, "basic_regression_solow_slides",tag,".pdf",sep=""),width=plot_width)
+#svg(paste(figures_dir, "basic_regression_solow_slides",tag,".svg",sep=""))
+par(mar=c(8,left_mar,4,right_mar)+0.1,cex.axis=1.2,cex.lab=1.5,xpd=TRUE)
+plot(array(0.0,dim=dim(reg_coefs)), ylim=c(0,1),lty='solid', lwd=thickness,col=col_complete,type='o', pch=15, las=1, xlab='N, Years of Growth',ylab='',main='Regressing Consumption Growth on Income Growth')
+lines(array(solow_spending,dim=dim(reg_coefs)),lty='solid', lwd=thickness,col=col_solow,type='o', pch=16)
+mtext(TeX('$\\beta^N$'),side=2,las=1,line=4, cex = 1.5)
+legend("right", inset=c(-0.6,0), legend=c("Complete Markets","Solow","",""), col=c(col_complete,col_solow,col_bs,"black"),lty=c("solid","solid","solid","solid"),lwd=c(thickness,thickness,thickness,thickness), pch=c(15,16,17,15), pt.cex = 1, cex = 1.3, y.intersp=1.3,bty="n")
+dev.off()
+
+#png(paste(figures_dir, "basic_regression_BS_slides",tag,".png",sep=""))
+pdf(paste(figures_dir, "basic_regression_BS_slides",tag,".pdf",sep=""),width=plot_width)
+#svg(paste(figures_dir, "basic_regression_BS_slides",tag,".svg",sep=""))
+par(mar=c(8,left_mar,4,right_mar)+0.1,cex.axis=1.2,cex.lab=1.5,xpd=TRUE)
+plot(array(0.0,dim=dim(reg_coefs)), ylim=c(0,1),lty='solid', lwd=thickness,col=col_complete,type='o', pch=15, las=1, xlab='N, Years of Growth',ylab='',main='Regressing Consumption Growth on Income Growth')
+lines(array(solow_spending,dim=dim(reg_coefs)),lty='solid', lwd=thickness,col=col_solow,type='o', pch=16)
+lines(FromPython[1:dim(reg_coefs)[1]],lty='solid',col=col_bs, lwd=thickness,type='o', pch=17)
+mtext(TeX('$\\beta^N$'),side=2,las=1,line=4, cex = 1.5)
+legend("right", inset=c(-0.6,0),legend=c("Complete Markets","Solow","Buffer-Stock",""), col=c(col_complete,col_solow,col_bs,"black"),lty=c("solid","solid","solid","solid"),lwd=c(thickness,thickness,thickness,thickness), pch=c(15,16,17,15), pt.cex = 1, cex = 1.3, y.intersp=1.3,bty="n")
+arrows(2, 0.12, 9, 0.12)
+arrows(9, 0.12, 2, 0.12)
+text(3.5, 0.22, labels = "Relatively more \n transitory variance")
+text(7.5, 0.22, labels = "Relatively more \n permanent variance")
+dev.off()
+
+#png(paste(figures_dir, "basic_regression_slides",tag,".png",sep=""))
+pdf(paste(figures_dir, "basic_regression_slides",tag,".pdf",sep=""),width=plot_width)
+#svg(paste(figures_dir, "basic_regression_slides",tag,".svg",sep=""))
+par(mar=c(8,left_mar,4,right_mar)+0.1,cex.axis=1.2,cex.lab=1.5,xpd=TRUE)
+plot(array(0.0,dim=dim(reg_coefs)), ylim=c(0,1),lty='solid', lwd=thickness,col=col_complete,type='o', pch=15, las=1, xlab='N, Years of Growth',ylab='',main='Regressing Consumption Growth on Income Growth')
+lines(array(solow_spending,dim=dim(reg_coefs)),lty='solid',col=col_solow, lwd=thickness,type='o', pch=16)
+lines(FromPython[1:dim(reg_coefs)[1]],lty='solid',lwd=thickness,col=col_bs,type='o', pch=17)
+points(reg_coefs, pch=15,lwd=thickness)
+lines(reg_coefs)
+lines(reg_coefs+1.96*std_errors,lty='dashed')
+lines(reg_coefs-1.96*std_errors,lty='dashed')
+mtext(TeX('$\\beta^N$'),side=2,las=1,line=4, cex = 1.5)
+legend("right", inset=c(-0.6,0), legend=c("Complete Markets","Solow","Buffer-Stock","Data"), col=c(col_complete,col_solow,col_bs,"black"),lty=c("solid","solid","solid","solid"),lwd=c(thickness,thickness,thickness,thickness), pch=c(15,16,17,15), pt.cex = 1, cex = 1.3, y.intersp=1.3,bty="n")
+arrows(2, 0.12, 9, 0.12)
+arrows(9, 0.12, 2, 0.12)
+text(3.5, 0.22, labels = "Relatively more \n transitory variance")
+text(7.5, 0.22, labels = "Relatively more \n permanent variance")
+dev.off()
+
+#png(paste(figures_dir, "basic_regression_liquid_wealth_slides",tag,".png",sep=""))
+pdf(paste(figures_dir, "basic_regression_liquid_wealth_slides",tag,".pdf",sep=""),width=plot_width)
+#svg(paste(figures_dir, "basic_regression_liquid_wealth",tag,".svg",sep=""))
+par(mar=c(8,left_mar,4,right_mar)+0.1,cex.axis=1.2,cex.lab=1.5,xpd=TRUE)
+plot(array(0.0,dim=dim(reg_coefs)), ylim=c(0,1),lty='solid', lwd=thickness,col=col_complete,type='o', pch=15, las=1, xlab='N, Years of Growth',ylab='',main='Regressing Consumption Growth on Income Growth')
+lines(array(solow_spending,dim=dim(reg_coefs)),lty='solid', lwd=thickness,col=col_solow,type='o', pch=16)
+lines(FromPython[1:dim(reg_coefs)[1]],lty='solid', lwd=thickness,col=col_bs,type='o', pch=17)
+points(reg_coefs,col='gray', pch=15,lwd=thickness)
+lines(reg_coefs,col='gray')
+lines(reg_coefs+1.96*std_errors,lty='dashed',col='gray')
+lines(reg_coefs-1.96*std_errors,lty='dashed',col='gray')
+#Add high and low liquid quantiles
+points(reg_coefs_liquid_wealth$X1, pch=15,lwd=thickness)
+lines(reg_coefs_liquid_wealth$X1)
+lines(reg_coefs_liquid_wealth$X1+1.96*std_errors_liquid_wealth$X1,lty='dashed')
+lines(reg_coefs_liquid_wealth$X1-1.96*std_errors_liquid_wealth$X1,lty='dashed')
+points(reg_coefs_liquid_wealth$X5, pch=15,lwd=thickness)
+lines(reg_coefs_liquid_wealth$X5)
+lines(reg_coefs_liquid_wealth$X5+1.96*std_errors_liquid_wealth$X5,lty='dashed')
+lines(reg_coefs_liquid_wealth$X5-1.96*std_errors_liquid_wealth$X5,lty='dashed')
+mtext(TeX('$\\beta^N$'),side=2,las=1,line=4, cex = 1.5)
+legend("right", inset=c(-0.6,0), legend=c("Complete Markets","Solow","Buffer-Stock","Data"), col=c(col_complete,col_solow,col_bs,"black"),lty=c("solid","solid","solid","solid"),lwd=c(thickness,thickness,thickness,thickness), pch=c(15,16,17,15), pt.cex = 1, cex = 1.3, y.intersp=1.3,bty="n")
+arrows(2, 0.12, 9, 0.12)
+arrows(9, 0.12, 2, 0.12)
+text(3.5, 0.22, labels = "Relatively more \n transitory variance")
+text(7.5, 0.22, labels = "Relatively more \n permanent variance")
+text(3.0, 0.92, labels = "Least Liquid", cex=1.3)
+text(6, 0.47, labels = "Most Liquid",cex=1.3)
+dev.off()
+
+##############################################################################
--- Code/Rcode/time_agg_random_walk_graph.R
+++ Code/Rcode/time_agg_random_walk_graph.R
@@ -1,5 +1,8 @@
 # Draw a time aggregated random walk (4 subperiods)
 
+library(ggplot2)
+library(gridExtra)
+
 figures_dir = "C:/Users/edmun/OneDrive/Documents/Research/Denmark/IncomeUncertaintyGit/Code/Rcode/Figures/"
 par(cex.axis=1.2,cex.lab=1.5)
 # 
@@ -60,6 +63,52 @@ lines(x,c, xlim= c(0,5),ylim=c(0,1.2),col="black",type="l",lwd=4,xlab="Time",yla
 lines(x,cBPP, xlim= c(0,5),ylim=c(0,1.2),col="green",type="l",lwd=4,xlab="Time",ylab="Income/Consumption",main="Generic Transitory Impulse Response, f(t)")
 legend(2.0,1.0, legend=c("Income f(t)", "Consumption g(t)","BPP Random Walk"),col=c("red","black","green"),lty=1,lwd=4,bty="n")
 dev.print(pdf,  paste(figures_dir,'GenericTransitoryConsumptionWithBPP.pdf',sep=""))
+
+#plots for the slides
+par(mar=c(5,5,3,3))
+plot(x,y, xlim= c(0,5),ylim=c(0,1.2),col="black",type="l",lwd=4,xlab="Years",ylab="Income",yaxt='n')
+legend(1.0,1.0, legend=c("f(t),  Path of Transitory Income Shock", ""),col=c("black","white"),lty=1,lwd=4,bty="n",cex=1.65)
+dev.print(pdf,  paste(figures_dir,'GenericTransitory_slides.pdf',sep=""))
+
+par(mar=c(5,5,3,3))
+plot(x,y, xlim= c(0,5),ylim=c(0,1.2),col="black",type="l",lwd=4,xlab="Years",ylab="Income/Consumption",yaxt='n')
+lines(x,c, xlim= c(0,5),ylim=c(0,1.2),col="red",type="l",lwd=4,xlab="Time",ylab="Income/Consumption",yaxt='n')
+legend(1.0,1.0, legend=c("f(t),  Path of Transitory Income Shock", "g(t), Path of Consuption Response"),col=c("black","red"),lty=1,lwd=4,bty="n", cex=1.65)
+dev.print(pdf,  paste(figures_dir,'GenericTransitoryConsumption_slides.pdf',sep=""))
+
+####################################
+# plot time aggregation stuff
+N_sub =2
+N_period =3
+N_subperiods =N_sub*N_period
+time = (1:(N_subperiods))/N_sub
+shocks = c(0,0,0,1,0,0)
+underlying = 50000+50000*cumsum(shocks)
+time_agg = underlying*0.0
+for (T in 1:N_period){
+  t = (T-1)*N_sub+1
+  time_agg[t:(t+N_sub-1)]=mean(underlying[t:(t+N_sub-1)])
+}
+
+time_agg_func=stepfun(time[1:(N_subperiods-1)], time_agg)
+underlying_func=stepfun(time[1:(N_subperiods-1)], underlying)
+
+dev.new()
+par(mar=c(5,7,2,2), mfcol=c(1,1),cex.axis=1.5,cex.lab=2)
+plot(underlying_func, time, xlim= c(0,N_period),ylim=c(0,110000),col="black",lty="solid",lwd=4, col.points=FALSE, verticals=TRUE,xlab="Time",main="",ylab="",yaxt = "n",las=1)
+axis(side = 2, at = c(0,25000,50000,75000,100000), las=1,label=c("$0","$25,000","$50,000","$75,000","$100,000"))
+legend(0.3,40000,legend=c("Permanent Income Flow",""),lty=c("solid","dashed"),lwd=c(4,4),col=c("black","white"),bty = "n", cex=1.7)
+dev.copy(pdf, paste(figures_dir, "TimeAggExample1_slides.pdf",sep=""))
+dev.off()
+
+dev.new()
+par(mar=c(5,7,2,2), mfcol=c(1,1),cex.axis=1.5,cex.lab=2)
+plot(underlying_func, time, xlim= c(0,N_period),ylim=c(0,110000),col="black",lty="solid",lwd=4, col.points=FALSE, verticals=TRUE,xlab="Time",main="",ylab="",yaxt = "n",las=1)
+axis(side = 2, at = c(0,25000,50000,75000,100000), las=1,label=c("$0","$25,000","$50,000","$75,000","$100,000"))
+lines(time_agg_func, time, xlim= c(0,N_period),ylim=c(0,110000),col="blue",lty="dashed",lwd=4, verticals=TRUE ,col.points=FALSE)
+legend(0.3,40000,legend=c("Permanent Income Flow","Observed Annual Income"),lty=c("solid","dashed"),lwd=c(4,4),col=c("black","blue"),bty = "n", cex=1.7)
+dev.copy(pdf, paste(figures_dir, "TimeAggExample2_slides.pdf",sep=""))
+dev.off()
 
 
 ######################################################################
