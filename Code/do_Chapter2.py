824658946bcf99b6996b3f1f7d964f3a8631f560
edmundcrawley <edmundcrawley@gmail.com>
2019-04-22 11:19:54 -0400

    Adds Pref shock model code to do_Chapter2.py

--- Code/do_Chapter2.py
+++ Code/do_Chapter2.py
@@ -1,6 +1,9 @@
 """
  Creates all tables and figures for Chapter 2 of my dissertation (Job Market Paper)
 """
+from __main__ import *
+import sys 
+import os
 import subprocess
 
 Rscript_path = "C:/Program Files/R/R-3.4.3/bin/x64/Rscript"
@@ -22,3 +25,15 @@ if r_status!=0:
 #if r_status!=0:
 #    print('R code could not run. Check the the path for Rscript')
 #    
+    
+print('Now running the MAIN model code')
+# Note the "do_labor_elas" tab controls whether the labor elasticity tables in the 
+# appendix are created. This is very sloooow (possibly days) and is set to "False"
+# as default
+sys.path.append(os.getcwd() + '/PrefShockModel/')
+os.chdir(os.getcwd() + '/PrefShockModel/')
+import MAIN
+
+print('Now create some remaining files')
+import PrintSummaryStats
+
